c2c750504f8fd573deb52fba384dd9caab49d396
psycolulz <112537894+psycolulz@users.noreply.github.com>
2022-10-21 17:02:57 -0400

    Update AutoMap.sh

--- AutoMap.sh
+++ AutoMap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-clear
-tput bold
-main() {
+command clear
+
+banner() {
     echo
     echo "NMAP CHEAT SHEET"
     echo "Created By: psycolulz"
@@ -13,76 +13,149 @@ echo "   _|_|                _|                _|      _|            "
 echo " _|    _|  _|    _|  _|_|_|_|    _|_|    _|_|  _|_|    _|_|_|  _|_|_|"    
 echo " _|_|_|_|  _|    _|    _|      _|    _|  _|  _|  _|  _|    _|  _|    _|"  
 echo " _|    _|  _|    _|    _|      _|    _|  _|      _|  _|    _|  _|    _|"  
-echo " _|    _|    _|_|_|      _|_|    _|_|    _|      _|    _|_|_|  _|_|_|  V. 1.0.0  " 
+echo " _|    _|    _|_|_|      _|_|    _|_|    _|      _|    _|_|_|  _|_|_|   " 
 echo "                                                               _|        "
 echo "                                                               _|      "  
     echo
     echo
-        PS3='root@automap~:'
-        options=("NMAP Wordpress Username Enum." "NMAP XSS Vuln Check" "NMAP DNS Bruteforce" "NMAP Get Header Information" "NMAP Port Scanning" "Update Tool")
-        select opt in "${options[@]}"
-         do
-         case $opt in
-         "NMAP Wordpress Username Enum.")
-         command clear
-         command nmap -sV --script http-wordpress-users --script-args -f limit=50 www.example.com
-         echo
-         echo
-         echo "Press Ctrl + C"
-         ;;
-         "NMAP XSS Vuln Check")
-         command clear
-         command nmap --script exploit -Pn -f www.example.com
-         echo
-         echo 
-         echo "Press Ctrl + C"
-         ;;
-         "NMAP DNS Bruteforce")
-         command clear 
-         command nmap -sP --script discovery -f www.example.com 
-         echo 
-         echo 
-         echo "Press Ctrl +C"
-         ;;
-         "NMAP Get Header Information")
-         command clear
-         command nmap -p80 --script http-useragent-tester.nse www.example.com 
-         echo 
-         echo
-         echo "Press Ctrl + C"
-         ;;
-         "NMAP Port Scanning")
-         command clear 
-         command nmap www.example.com 
-         echo 
-         echo 
-         echo "Press Ctrl +C"
-         ;;
-         "Update Tool")
-         command clear 
-         command git pull 
-         command clear 
-         echo 
-         echo "Tool Has Been Updated Successfully!"
-         echo " ⠀⠀⠀⠀⠀         ⠀⠀⢀⣴⣿⣿⣿⣷⣶⣴⣾⣿⣿⣿⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀
-         ⠀⠀⠀⠀⣀⣤⣤⣴⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣤⣤⣤⣄⠀⠀⠀⠀
-         ⠀⠀⠀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⠀⠀⠀
-         ⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡀⠀⠀
-         ⢀⣤⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦⡀
-         ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠁⠈⢻⣿⣿⣿⣿⣿⣿⣿
-         ⢿⣿⣿⣿⣿⣿⣿⣿⡿⠻⣿⣿⣿⣿⣿⣿⣿⠟⠁⠀⢀⣴⣿⣿⣿⣿⣿⣿⣿⣿
-         ⢈⣿⣿⣿⣿⣿⣿⣯⡀⠀⠈⠻⣿⣿⣿⠟⠁⠀⢀⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⡁
-         ⣾⣿⣿⣿⣿⣿⣿⣿⣿⣦⡀⠀⠈⠛⠁⠀⢀⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷
-         ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦⡀⠀⢀⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
-         ⠈⠛⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠁
-         ⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠇⠀⠀
-         ⠀⠀⠀⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠀⠀
-          ⠀⠀⠀⠉⠛⠛⠛⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠛⠛⠉⠁⠀⠀⠀
-         ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠻⣿⣿⣿⠿⢿⡻⠿⣿⣿⣿⠟⠁⠀⠀⠀
-"
-echo "Press Ctrl + C"⠀
-;;
-         esac
-         done
+
 }
-main
+
+    menu() {
+        echo
+    echo "1 Wordpress Username Enumeration"
+    echo "2 NMAP XSS Vulnerability Check"
+    echo "3 NMAP DNS Bruteforce"
+    echo "4 NMAP Get Header Information"
+    echo "5 NMAP Port Scanning"
+    echo "6 Update Tool"
+    read -p $'Choose an option:' choice
+
+    if [[ $choice == "1" ]] then
+    wordpressenum
+    elif [[ $choice == "2" ]] then
+    xssvuln
+    elif [[ $choice == "3" ]] then
+    dnsbrute
+    elif [[ $choice == "4" ]] then
+    headerinfo
+    elif [[ $choice == "5" ]] then 
+    portscan
+    elif [[ $choice == "6" ]] then
+    update 
+    else
+    clear
+    banner
+    menu
+    fi
+    }
+
+
+    wordpressenum() {
+command clear 
+    echo
+    echo
+    echo "Enter Hostname Without https://"
+    echo
+    read hostname
+     command nmap -sV --script http-wordpress-users --script-args -f $hostname
+     printf "\n"
+     echo "Sleep Is Set To 100, Exit Tool To Restart"
+     sleep 100
+     command clear
+     banner
+     menu
+     }
+     
+     xssvuln() {
+     command clear
+     echo
+     echo
+     echo "Enter Hostname Without https://"
+     echo
+     read hostname
+     command nmap --script exploit -Pn -f $hostname
+     echo
+     echo 
+     printf "Sleep Is Set To 100, Exit Tool To Restart"
+     sleep 100
+     command clear
+     banner
+     menu
+     }
+     
+     dnsbrute() {
+     command clear
+     echo
+     echo
+     echo "Enter Hostname Without https://"
+     echo
+     read hostname 
+     command nmap -sP --script discovery -f $hostname 
+     printf "\n" 
+     printf "\n" 
+     printf "Sleep Is Set To 100, Exit Tool To Restart"
+     sleep 100
+     command clear
+     banner
+     menu
+     }
+     
+     headerinfo() {
+     command clear
+     echo
+     echo
+     echo "Enter Hostname Without https://"
+     echo
+     read hostname
+     command nmap -p80 --script http-useragent-tester.nse $hostname 
+     echo 
+     echo
+     echo "Sleep Is Set To 100, Exit Tool To Restart"
+     sleep 100
+     command clear
+     banner
+     menu
+     }
+     portscan() {
+     command clear 
+     echo
+     echo
+     echo "Enter Hostname Without https://"
+     echo
+     read hostname
+     command nmap $hostname
+     echo
+     echo 
+     echo "Sleep Is Set To 100, Exit Tool To Restart"
+     sleep 100
+     command clear
+     banner
+     menu
+     }
+     update() {
+     command clear 
+     command git pull 
+     command clear 
+     echo 
+     echo "Tool Has Been Updated Successfully!"
+     echo  "              ⢀⣴⣿⣿⣿⣷⣶⣴⣾⣿⣿⣿⣦⡀"⠀⠀⠀⠀⠀⠀⠀⠀
+     echo  "         ⣀⣤⣤⣴⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣤⣤⣤⣄"⠀⠀⠀⠀
+     echo  "⠀ ⠀   ⠀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷"⠀⠀⠀
+     echo  "  ⠀   ⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿."⠀⠀
+     echo  "   ⢀⣤⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦⡀"
+     echo  "   ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠁⠈⢻⣿⣿⣿⣿⣿⣿⣿"
+     echo  "   ⢿⣿⣿⣿⣿⣿⣿⣿⡿⠻⣿⣿⣿⣿⣿⣿⣿⠟⠁⠀⢀⣴⣿⣿⣿⣿⣿⣿⣿⣿"
+     echo  "   ⢈⣿⣿⣿⣿⣿⣿⣯⡀⠀⠈⠻⣿⣿⣿⠟⠁⠀⢀⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⡁"
+     echo  "   ⣾⣿⣿⣿⣿⣿⣿⣿⣿⣦⡀⠀⠈⠛⠁⠀⢀⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷"
+     echo  "   ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦⡀⠀⢀⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿"
+     echo  "    ⠛⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠁"
+     echo⠀ "       ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠇"⠀⠀
+     echo⠀⠀"      ⠀⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠀"⠀
+     echo ⠀"        ⠉⠛⠛⠛⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠛⠛⠉⠁"⠀⠀⠀
+     echo⠀⠀"⠀           ⠀⠀⠻⣿⣿⣿⠿⢿⡻⠿⣿⣿⣿⠟⠁"⠀⠀⠀
+     echo "Sleep Is Set To 100, Exit To Restart"
+     sleep 100
+    }  
+    banner
+    menu
